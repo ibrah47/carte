61d35f7033074ab39e977f226c6e4fe4ca059a28
ibasow90 <31648581-ibasow90@users.noreply.replit.com>
2024-01-28 20:25:42 +0000

    Étape 2: Simulation des Mouvements des Aventuriers

--- carte-aux-tresors/main.js
+++ carte-aux-tresors/main.js
@@ -57,6 +57,170 @@ function lireFichierEntree(cheminFichier) {
   return { carte, aventuriers };
 }
 
+function simulerMouvements(carte, aventuriers) {
+  aventuriers.forEach((aventurier) => {
+    for (let mouvement of aventurier.sequenceMouvement) {
+      switch (mouvement) {
+        case "A":
+          deplacerAventurier(carte, aventurier);
+          collecterTresor(carte, aventurier);
+          break;
+
+        case "G":
+          tournerAGauche(aventurier);
+          break;
+
+        case "D":
+          tournerADroite(aventurier);
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
+}
+
+function deplacerAventurier(carte, aventurier) {
+  const nouvellePosition = { x: aventurier.x, y: aventurier.y };
+
+  switch (aventurier.orientation) {
+    case "N":
+      nouvellePosition.y -= 1;
+      break;
+
+    case "S":
+      nouvellePosition.y += 1;
+      break;
+
+    case "E":
+      nouvellePosition.x += 1;
+      break;
+
+    case "O":
+      nouvellePosition.x -= 1;
+      break;
+
+    default:
+      break;
+  }
+
+  if (estPositionValide(carte, nouvellePosition)) {
+    aventurier.x = nouvellePosition.x;
+    aventurier.y = nouvellePosition.y;
+  }
+}
+
+function estPositionValide(carte, position) {
+  if (
+    position.x >= 0 &&
+    position.y >= 0 &&
+    position.x < carte[0].largeur &&
+    position.y < carte[0].hauteur
+  ) {
+    const estMontagne = carte.some(
+      (element) =>
+        element.type === "M" &&
+        element.x === position.x &&
+        element.y === position.y,
+    );
+
+    return !estMontagne;
+  }
+
+  return false;
+}
+
+function tournerAGauche(aventurier) {
+  switch (aventurier.orientation) {
+    case "N":
+      aventurier.orientation = "O";
+      break;
+
+    case "S":
+      aventurier.orientation = "E";
+      break;
+
+    case "E":
+      aventurier.orientation = "N";
+      break;
+
+    case "O":
+      aventurier.orientation = "S";
+      break;
+
+    default:
+      break;
+  }
+}
+
+function tournerADroite(aventurier) {
+  switch (aventurier.orientation) {
+    case "N":
+      aventurier.orientation = "E";
+      break;
+
+    case "S":
+      aventurier.orientation = "O";
+      break;
+
+    case "E":
+      aventurier.orientation = "S";
+      break;
+
+    case "O":
+      aventurier.orientation = "N";
+      break;
+
+    default:
+      break;
+  }
+}
+
+function collecterTresor(carte, aventurier) {
+  const caseAventurier = carte.find(
+    (element) =>
+      element.type === "T" &&
+      element.x === aventurier.x &&
+      element.y === aventurier.y,
+  );
+
+  if (caseAventurier) {
+    aventurier.nbTresorsRamasses += Math.min(caseAventurier.nbTresors, 1);
+
+    caseAventurier.nbTresors = Math.max(0, caseAventurier.nbTresors - 1);
+  }
+}
+
+// Logique de simulation des mouvements
+aventuriers.forEach((aventurier) => {
+  for (let mouvement of aventurier.sequenceMouvement) {
+    switch (mouvement) {
+      case "A":
+        deplacerAventurier(carte, aventurier);
+        collecterTresor(carte, aventurier);
+        break;
+
+      case "G":
+        tournerAGauche(aventurier);
+        break;
+
+      case "D":
+        tournerADroite(aventurier);
+        break;
+
+      default:
+        // Ignorer les mouvements invalides
+        break;
+    }
+  }
+});
+
 const { carte, aventuriers } = lireFichierEntree("input.txt");
+
+// Logique de simulation des mouvements
+simulerMouvements(carte, aventuriers);
+
+// Afficher le résultat
 console.log(carte);
 console.log(aventuriers);
