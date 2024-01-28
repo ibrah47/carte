6eeac51665e229500b77a5d25c87a181f3c6e628
ibasow90 <31648581-ibasow90@users.noreply.replit.com>
2024-01-28 21:06:34 +0000

    Étape 3: Écriture du fichier de Sortie

--- carte-aux-tresors/main.js
+++ carte-aux-tresors/main.js
@@ -1,226 +1,232 @@
 const fs = require("fs");
 
-function lireFichierEntree(cheminFichier) {
-  const lignes = fs.readFileSync(cheminFichier, "utf8").split("\n");
-  const carte = [];
-  const aventuriers = [];
+function lireFichierEntree(nomFichierEntree) {
+  console.log(`Lecture du fichier: ${nomFichierEntree}`);
+  const contenu = fs.readFileSync(nomFichierEntree, "utf-8");
+  const lignes = contenu.split("\n");
+  const donnees = {
+    carte: [],
+    montagnes: [],
+    tresors: [],
+    aventuriers: [],
+  };
 
-  lignes.forEach((ligne) => {
-    if (ligne.startsWith("#")) {
-      return;
-    }
+  for (const ligne of lignes) {
+    if (ligne.trim() !== "" && !ligne.startsWith("#")) {
+      const elements = ligne.trim().split(/\s+/);
+      const type = elements[0];
 
-    const elements = ligne.split(" - ");
-
-    switch (elements[0]) {
-      case "C":
-        carte.push({
-          type: "C",
-          largeur: parseInt(elements[2]),
-          hauteur: parseInt(elements[3]),
-        });
-        break;
-
-      case "M":
-        carte.push({
-          type: "M",
-          x: parseInt(elements[2]),
-          y: parseInt(elements[3]),
-        });
-        break;
-
-      case "T":
-        carte.push({
-          type: "T",
-          x: parseInt(elements[2]),
-          y: parseInt(elements[3]),
-          nbTresors: parseInt(elements[4]),
-        });
-        break;
-
-      case "A":
-        aventuriers.push({
-          type: "A",
-          nom: elements[1],
-          x: parseInt(elements[2]),
-          y: parseInt(elements[3]),
-          orientation: elements[4],
-          sequenceMouvement: elements[5],
-        });
-        break;
-
-      default:
-        break;
-    }
-  });
-
-  return { carte, aventuriers };
-}
-
-function simulerMouvements(carte, aventuriers) {
-  aventuriers.forEach((aventurier) => {
-    for (let mouvement of aventurier.sequenceMouvement) {
-      switch (mouvement) {
+      switch (type) {
+        case "C":
+          donnees.carte = elements.slice(1).map((e) => convertirCoordonnee(e));
+          break;
+        case "M":
+          donnees.montagnes.push(
+            elements.slice(1).map((e) => convertirCoordonnee(e)),
+          );
+          break;
+        case "T":
+          const axeX = convertirCoordonnee(elements[1]);
+          const axeY = convertirCoordonnee(elements[2]);
+          const nbTresors = convertirCoordonnee(elements[3]);
+          console.log("Coordonnées trésor:", axeX, axeY, nbTresors);
+          donnees.tresors.push(
+            convertirCoordonneesTresor(...ligne.split(" ").slice(2)),
+          );
+          break;
         case "A":
-          deplacerAventurier(carte, aventurier);
-          collecterTresor(carte, aventurier);
+          donnees.aventuriers.push(elements.slice(1));
           break;
-
-        case "G":
-          tournerAGauche(aventurier);
-          break;
-
-        case "D":
-          tournerADroite(aventurier);
-          break;
-
         default:
           break;
       }
     }
-  });
-}
-
-function deplacerAventurier(carte, aventurier) {
-  const nouvellePosition = { x: aventurier.x, y: aventurier.y };
-
-  switch (aventurier.orientation) {
-    case "N":
-      nouvellePosition.y -= 1;
-      break;
-
-    case "S":
-      nouvellePosition.y += 1;
-      break;
-
-    case "E":
-      nouvellePosition.x += 1;
-      break;
-
-    case "O":
-      nouvellePosition.x -= 1;
-      break;
-
-    default:
-      break;
   }
 
-  if (estPositionValide(carte, nouvellePosition)) {
-    aventurier.x = nouvellePosition.x;
-    aventurier.y = nouvellePosition.y;
-  }
+  console.log("Données lues:", donnees);
+  return donnees;
 }
 
-function estPositionValide(carte, position) {
-  if (
-    position.x >= 0 &&
-    position.y >= 0 &&
-    position.x < carte[0].largeur &&
-    position.y < carte[0].hauteur
-  ) {
-    const estMontagne = carte.some(
-      (element) =>
-        element.type === "M" &&
-        element.x === position.x &&
-        element.y === position.y,
+function simulerMouvementsAventuriers(donnees) {
+  const { carte, montagnes, tresors, aventuriers } = donnees;
+
+  for (const aventurierData of aventuriers) {
+    const [_, nom, x, __, y, ___, orientation, _____, mouvements] =
+      aventurierData;
+    const aventurier = {
+      nom,
+      position: { x: convertirCoordonnee(x), y: convertirCoordonnee(y) },
+      orientation,
+      mouvements: mouvements === "-" ? "" : mouvements,
+      tresorsRamasses: 0,
+    };
+
+    console.log(
+      `Avant mouvement - ${nom} - Aventurier ${nom} - position: `,
+      aventurier.position,
     );
-
-    return !estMontagne;
-  }
-
-  return false;
-}
-
-function tournerAGauche(aventurier) {
-  switch (aventurier.orientation) {
-    case "N":
-      aventurier.orientation = "O";
-      break;
-
-    case "S":
-      aventurier.orientation = "E";
-      break;
-
-    case "E":
-      aventurier.orientation = "N";
-      break;
-
-    case "O":
-      aventurier.orientation = "S";
-      break;
-
-    default:
-      break;
+    simulerMouvements(aventurier, carte, montagnes, tresors);
+    console.log(
+      `Après mouvement - ${nom} - Aventurier ${nom} - position: `,
+      aventurier.position,
+    );
   }
 }
 
-function tournerADroite(aventurier) {
-  switch (aventurier.orientation) {
-    case "N":
-      aventurier.orientation = "E";
-      break;
+function simulerMouvements(aventurier, carte, montagnes, tresors) {
+  const { x, y } = aventurier.position;
+  const { orientation, mouvements } = aventurier;
 
-    case "S":
-      aventurier.orientation = "O";
-      break;
-
-    case "E":
-      aventurier.orientation = "S";
-      break;
-
-    case "O":
-      aventurier.orientation = "N";
-      break;
-
-    default:
-      break;
-  }
-}
-
-function collecterTresor(carte, aventurier) {
-  const caseAventurier = carte.find(
-    (element) =>
-      element.type === "T" &&
-      element.x === aventurier.x &&
-      element.y === aventurier.y,
-  );
-
-  if (caseAventurier) {
-    aventurier.nbTresorsRamasses += Math.min(caseAventurier.nbTresors, 1);
-
-    caseAventurier.nbTresors = Math.max(0, caseAventurier.nbTresors - 1);
-  }
-}
-
-// Logique de simulation des mouvements
-aventuriers.forEach((aventurier) => {
-  for (let mouvement of aventurier.sequenceMouvement) {
+  for (const mouvement of mouvements) {
     switch (mouvement) {
       case "A":
-        deplacerAventurier(carte, aventurier);
-        collecterTresor(carte, aventurier);
+        deplacerAventurier(aventurier, carte, montagnes);
+        ramasserTresor(aventurier, tresors);
         break;
-
       case "G":
-        tournerAGauche(aventurier);
+        aventurier.orientation = tournerAGauche(orientation);
         break;
-
       case "D":
-        tournerADroite(aventurier);
+        aventurier.orientation = tournerADroite(orientation);
         break;
-
       default:
-        // Ignorer les mouvements invalides
+        // Ignorer les mouvements inconnus
         break;
     }
   }
-});
+}
 
-const { carte, aventuriers } = lireFichierEntree("input.txt");
+function deplacerAventurier(aventurier, carte, montagnes) {
+  const { x, y } = aventurier.position;
+  const { orientation } = aventurier;
 
-// Logique de simulation des mouvements
-simulerMouvements(carte, aventuriers);
+  switch (orientation) {
+    case "N":
+      if (y > 0 && !estMontagne(x, y - 1, montagnes)) {
+        aventurier.position.y--;
+      }
+      break;
+    case "S":
+      if (y < carte[1] - 1 && !estMontagne(x, y + 1, montagnes)) {
+        aventurier.position.y++;
+      }
+      break;
+    case "E":
+      if (x < carte[0] - 1 && !estMontagne(x + 1, y, montagnes)) {
+        aventurier.position.x++;
+      }
+      break;
+    case "O":
+      if (x > 0 && !estMontagne(x - 1, y, montagnes)) {
+        aventurier.position.x--;
+      }
+      break;
+    default:
+      // Ignorer les orientations inconnues
+      break;
+  }
+}
 
-// Afficher le résultat
-console.log(carte);
-console.log(aventuriers);
+function ramasserTresor(aventurier, tresors) {
+  const { x, y } = aventurier.position;
+
+  for (let i = 0; i < tresors.length; i++) {
+    const tresor = tresors[i];
+    if (tresor.x === x && tresor.y === y) {
+      aventurier.tresorsRamasses++;
+      tresor.nbTresors--;
+      if (tresor.nbTresors === 0) {
+        tresors.splice(i, 1);
+      }
+      break; // Un aventurier peut ramasser au plus un trésor par case
+    }
+  }
+}
+
+function estMontagne(x, y, montagnes) {
+  for (const montagne of montagnes) {
+    const [mx, my] = montagne;
+    if (mx === x && my === y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function tournerAGauche(orientation) {
+  switch (orientation) {
+    case "N":
+      return "O";
+    case "S":
+      return "E";
+    case "E":
+      return "N";
+    case "O":
+      return "S";
+    default:
+      return orientation;
+  }
+}
+
+function tournerADroite(orientation) {
+  switch (orientation) {
+    case "N":
+      return "E";
+    case "S":
+      return "O";
+    case "E":
+      return "S";
+    case "O":
+      return "N";
+    default:
+      return orientation;
+  }
+}
+
+function convertirCoordonnee(coordonnee) {
+  const valeurParsee = parseInt(coordonnee);
+  return isNaN(valeurParsee) ? null : valeurParsee;
+}
+
+function convertirAventuriers(nom, x, y, orientation, mouvements) {
+  console.log(`Aventurier ${nom} - position: { x: ${x}, y: ${y} }`);
+
+  const coordX = convertirCoordonnee(x);
+  const coordY = convertirCoordonnee(y);
+
+  if (!isNaN(coordX) && !isNaN(coordY)) {
+    return {
+      nom,
+      position: { x: coordX, y: coordY },
+      orientation,
+      mouvements,
+      tresorsRamasses: 0,
+    };
+  } else {
+    console.log(`Coordonnées aventurier ${nom} invalides`);
+    return null;
+  }
+}
+
+function convertirCoordonneesTresor(x, y, nbTresors) {
+  return {
+    x: convertirCoordonnee(x),
+    y: y === null ? null : convertirCoordonnee(y),
+    nbTresors: convertirCoordonnee(nbTresors),
+  };
+}
+
+function ecrireFichierSortie(nomFichierSortie, donnees) {
+  const contenuSortie = donnees.carte
+    .concat(donnees.montagnes, donnees.tresors, donnees.aventuriers)
+    .join("\n");
+  fs.writeFileSync(nomFichierSortie, contenuSortie, "utf-8");
+  console.log("Résultats écrits dans le fichier:", nomFichierSortie);
+}
+
+const nomFichierEntree = "input.txt";
+const nomFichierSortie = "output.txt";
+
+const donnees = lireFichierEntree(nomFichierEntree);
+simulerMouvementsAventuriers(donnees);
+ecrireFichierSortie(nomFichierSortie, donnees);
